fe74ce9ddde268c16fe59fd2e2e17c30e818c5a6
yuanchao@egova.com.cn <yuanchao@egova.com.cn>
2022-02-05 11:09:25 +0800

    [M]自定义状态码解析规则

---
+++ examples/status/app.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosError } from '../../src/index'
+
+axios
+  .get('/more/304')
+  .then(res => {
+    console.log(res)
+  })
+  .catch((e: AxiosError) => {
+    console.log(e.message)
+  })
+
+axios
+  .get('/more/304', {
+    validateStatus(status) {
+      return status >= 200 && status < 400
+    }
+  })
+  .then(res => {
+    console.log(res)
+  })
+  .catch((e: AxiosError) => {
+    console.log(e.message)
+  })
